7acdef2b43112289f3db665683a195f35505c0e9
mirror12k <mirror12k@gmail.com>
2024-09-10 10:35:49 -0400

    - trying to fix build

--- weasel_make/weasel.py
+++ weasel_make/weasel.py
@@ -82,11 +82,7 @@ def load_makefile(filepath):
 def execute_shell_command(command, log_length=40):
 	# run the command with pipefail on
 	# nosemgrep
-	proc = subprocess.Popen('bash -o pipefail -c "' + command + '" 2>&1',
-			shell=True,
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE,
-			universal_newlines=True)
+	proc = subprocess.Popen('bash -o pipefail -c "' + command + '" 2>&1', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
 	last_lines = []
 
